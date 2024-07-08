70f9f6af23eebeafcb237bc55659198e22d522ed
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-08 13:43:12 -0600

    is greeting the world with enthusiasm

---
+++ src/main/java/edu/velv/wikidata_universe_api/WikidataUniverseApiApplication.java
@@ -0,0 +1,13 @@
+package edu.velv.wikidata_universe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikidataUniverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikidataUniverseApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/velv/wikidata_universe_api/models/HelloWorld.java
@@ -0,0 +1,4 @@
+package edu.velv.wikidata_universe_api.models;
+
+public record HelloWorld(long id, String content) {
+}
