23fc8a449640bcbfb9a836f8c1b4708caca81f1c
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-12 21:29:11 -0600

    adds random 3D point initter and tests, working as expected

---
+++ src/main/java/edu/velv/wikidata_universe_api/models/jung/RandomLocation3D.java
@@ -0,0 +1,23 @@
+package edu.velv.wikidata_universe_api.models.jung;
+
+import java.awt.Dimension;
+import java.util.Date;
+import java.util.Random;
+
+import com.google.common.base.Function;
+
+public class RandomLocation3D<V> implements Function<V, Point3D> {
+  private Dimension dimension;
+  private Random random;
+
+  public RandomLocation3D(Dimension dimension) {
+    this.dimension = dimension;
+    this.random = new Random(new Date().getTime());
+  }
+
+  @Override
+  public Point3D apply(V input) {
+    return new Point3D(random.nextDouble() * dimension.width, random.nextDouble() * dimension.height,
+        random.nextDouble() * dimension.height);
+  }
+}
