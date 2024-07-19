3ea42ada8bd6d2514e3493247fa5be3ac5a5456a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-19 13:20:15 +0100

    to webflux use built in response starts

--- src/main/java/edu/velv/wikidata_universe_api/controllers/APIStatusController.java
+++ src/main/java/edu/velv/wikidata_universe_api/controllers/APIStatusController.java
@@ -1,17 +1,15 @@
 package edu.velv.wikidata_universe_api.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import edu.velv.wikidata_universe_api.models.api.RequestResponse.StatusResponse;
-
 
 @RestController
 public class APIStatusController {
   @CrossOrigin
   @GetMapping("/api/status")
-  public StatusResponse status() {
-    return new StatusResponse();
+  public ResponseEntity<String> status() {
+    return ResponseEntity.ok("API Online.");
   }
 }
