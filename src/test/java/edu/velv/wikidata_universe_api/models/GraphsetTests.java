683ddbad61a4be43c309bc4dcf2299b93a04b4e8
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-23 11:25:43 -0600

    remove old paradyme test

--- src/test/java/edu/velv/wikidata_universe_api/models/GraphsetTests.java
+++ src/test/java/edu/velv/wikidata_universe_api/models/GraphsetTests.java
@@ -5,11 +5,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class GraphsetTests {
-
-  @Test
-  public void init_Graphset() {
-    Graphset gs = new Graphset("Kevin Bacon");
-
-    assertEquals("Kevin Bacon", gs.originalQuery());
-  }
 }
