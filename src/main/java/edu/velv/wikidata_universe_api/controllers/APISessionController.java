013b2c915eba8b9e4a904935b2b91074448b9e2a
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-24 21:20:20 -0600

    remove v1 from pathing, using to build out init session in progress

--- src/main/java/edu/velv/wikidata_universe_api/controllers/APISessionController.java
+++ src/main/java/edu/velv/wikidata_universe_api/controllers/APISessionController.java
@@ -12,22 +12,22 @@ import edu.velv.wikidata_universe_api.models.utils.QueryParamSanitizer;
 @CrossOrigin
 @RestController
 public class APISessionController {
-  @GetMapping("/api/v1/init-session")
+  @GetMapping("/api/init-session")
   public ResponseEntity<String> initSession(
       @RequestParam(value = "query", defaultValue = "Kevin Bacon") String query,
-      @RequestParam(value = "dimensions", defaultValue = "1600x900") String dimensions) {
+      @RequestParam(value = "dimensions", defaultValue = "900x900") String dimensions) {
     query = QueryParamSanitizer.sanitize(query);
     ClientSession session = new ClientSession(query, dimensions);
     // Subject emits init event => new session create, needs dimesnions & query
     return ResponseEntity.ok("Session Initializing[query=" + query + ", dimensions=" + dimensions + "]");
   }
 
-  @GetMapping("/api/v1/update-session")
+  @GetMapping("/api/update-session")
   public void updateSessionDimesnions() {
     // Subject emits windowResize event => update session dimesnions to run new layout
   }
 
-  @GetMapping("/api/v1/update-graphset")
+  @GetMapping("/api/update-graphset")
   public void updateGraphsetOnExplore() {
     // Subject emits click event => update graphset w/ newly fetched ents 
   }
