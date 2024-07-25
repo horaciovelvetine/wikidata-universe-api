174b5070ff46591f4652fac9a2e81a6747de3a14
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-24 21:14:11 -0600

    include label and typing for ingest process

--- src/main/java/edu/velv/wikidata_universe_api/models/Edge.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/Edge.java
@@ -1,12 +1,19 @@
 package edu.velv.wikidata_universe_api.models;
 
+import edu.velv.wikidata_universe_api.models.wikidata.SnakData;
+import edu.velv.wikidata_universe_api.models.wikidata.ValueData.ValueType;
+
 public class Edge {
   private String srcEntId;
   private String tgtEntId;
   private String propertyId;
+  private String label;
+  private ValueType type;
 
-  public Edge() {
-    // Default constructor
+  public Edge(String srcVertexId, SnakData mainSnak) {
+    this.srcEntId = srcVertexId;
+    this.propertyId = mainSnak.property.value;
+
   }
 
   public String srcEntId() {
@@ -19,6 +26,13 @@ public class Edge {
 
   public String propertyId() {
     return propertyId;
+  }
+
+  public String label() {
+    return label;
+  }
+  public ValueType type() {
+    return type;
   }
 
   public void setSrcEntId(String srcEntId) {
