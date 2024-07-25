d97091db533b0662119e8e62a13b9b12cfe6f27d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-24 21:13:54 -0600

    init basic needed graphset storage structs

--- src/main/java/edu/velv/wikidata_universe_api/models/Graphset.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/Graphset.java
@@ -1,13 +1,48 @@
 package edu.velv.wikidata_universe_api.models;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Graphset {
   private String originalQuery;
 
-  public Graphset(String originalQuery) {
-    this.originalQuery = originalQuery;
+  private Set<Vertex> vertices;
+  private Set<Edge> edges;
+  private Set<Property> properties;
+
+  public Graphset() {
+    this.vertices = ConcurrentHashMap.newKeySet();
+    this.edges = ConcurrentHashMap.newKeySet();
+    this.properties = ConcurrentHashMap.newKeySet();
   }
 
   public String originalQuery() {
     return originalQuery;
+  }
+
+  public Set<Vertex> vertices() {
+    return vertices;
+  }
+
+  public Set<Edge> edges() {
+    return edges;
+  }
+
+  public Set<Property> properties() {
+    return properties;
+  }
+
+  public void addVertex(Vertex v) {
+    if (vertices().contains(v)) {
+      return;
+    }
+    vertices.add(v);
+  }
+
+  public void addEdge(Edge e) {
+    if (edges().contains(e)) {
+      return;
+    }
+    edges.add(e);
   }
 }
