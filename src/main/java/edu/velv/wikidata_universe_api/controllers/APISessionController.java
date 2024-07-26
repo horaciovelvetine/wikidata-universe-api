2de4b3ecd176af396ba2cfaacc682f83c98b28ba
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-25 23:44:03 -0600

    inits top level error claim on static ref to initialize session, should allow returning bad news all the way to the top and letting the cx know

--- src/main/java/edu/velv/wikidata_universe_api/controllers/APISessionController.java
+++ src/main/java/edu/velv/wikidata_universe_api/controllers/APISessionController.java
@@ -1,5 +1,7 @@
 package edu.velv.wikidata_universe_api.controllers;
 
+import io.vavr.control.Either;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.velv.wikidata_universe_api.models.ClientSession;
-import edu.velv.wikidata_universe_api.models.utils.QueryParamSanitizer;
+import edu.velv.wikidata_universe_api.models.err.SessionControllerErrorResponse;
+import edu.velv.wikidata_universe_api.models.err.WikiverseError;
 
 @CrossOrigin
 @RestController
@@ -15,10 +18,10 @@ public class APISessionController {
   @GetMapping("/api/init-session")
   public ResponseEntity<String> initSession(
       @RequestParam(required = true) String query, @RequestParam(required = true) String dimensions) {
-    query = QueryParamSanitizer.sanitize(query);
-    ClientSession session = new ClientSession(query, dimensions);
-    // Subject emits init event => new session create, needs dimesnions & query
-    return ResponseEntity.ok("Session Initializing[query=" + query + ", dimensions=" + dimensions + "]");
+    Either<WikiverseError, ClientSession> session = ClientSession.initialize(query, dimensions);
+
+    return session.mapLeft(SessionControllerErrorResponse::mapWikiverseErrorToResponse)
+        .fold(this::buildErrorResponse, this::buildSuccessResponse);
   }
 
   @GetMapping("/api/update-session")
@@ -29,5 +32,13 @@ public class APISessionController {
   @GetMapping("/api/update-graphset")
   public void updateGraphsetOnExplore() {
     // Subject emits click event => update graphset w/ newly fetched ents 
+  }
+
+  private ResponseEntity<String> buildSuccessResponse(Object body) {
+    return ResponseEntity.status(200).body(body.toString());
+  }
+
+  private ResponseEntity<String> buildErrorResponse(SessionControllerErrorResponse err) {
+    return ResponseEntity.status(err.status()).body(err.message());
   }
 }
