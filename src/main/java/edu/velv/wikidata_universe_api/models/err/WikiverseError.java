fff6c0ec82d6c8192fb5ba7436a1ac5f04029e7f
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-25 23:44:24 -0600

    changes reflect use so far

--- src/main/java/edu/velv/wikidata_universe_api/models/err/WikiverseError.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/err/WikiverseError.java
@@ -1,15 +1,16 @@
 package edu.velv.wikidata_universe_api.models.err;
 
-public sealed interface WikiverseError permits WikiverseError.WikidataServiceError {
+public sealed interface WikiverseError
+    permits WikiverseError.WikidataServiceError {
 
   public sealed interface WikidataServiceError extends WikiverseError {
-    record ApiRequestFailed(String message) implements WikidataServiceError {
+    record ApiRequestFailed(String message, Throwable cause) implements WikidataServiceError {
     }
 
     record NoSuchEntityFound(String message) implements WikidataServiceError {
     }
 
-    record UnexpectedError(String message) implements WikidataServiceError {
+    record UnexpectedError(String message, Throwable cause) implements WikidataServiceError {
     }
   }
 }
