e1573d986d24b583d1d0f2bb489f6282c93baf8e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-25 23:52:59 -0600

    ultimately finalize swap to either to use errors as values

--- src/main/java/edu/velv/wikidata_universe_api/models/wikidata/FetchBroker.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/wikidata/FetchBroker.java
@@ -1,76 +1,150 @@
 package edu.velv.wikidata_universe_api.models.wikidata;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import io.vavr.CheckedFunction0;
+import io.vavr.control.Either;
+import io.vavr.control.Try;
+
+import org.wikidata.wdtk.datamodel.helpers.ItemDocumentBuilder;
+import org.wikidata.wdtk.datamodel.helpers.PropertyDocumentBuilder;
+import org.wikidata.wdtk.datamodel.implementation.ItemIdValueImpl;
+import org.wikidata.wdtk.datamodel.implementation.PropertyIdValueImpl;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
+import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
+import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
 import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
+import org.wikidata.wdtk.datamodel.interfaces.DatatypeIdValue;
+
+import edu.velv.wikidata_universe_api.models.err.WikiverseError;
 
 public class FetchBroker {
-  private WikibaseDataFetcher wbdf;
+  private static final String WIKI_SITE_KEY = "enwiki";
+  private static final String DEFAULT_FALLBACK_ID = "Q3454165";
+  private final WikibaseDataFetcher fetcher;
 
   public FetchBroker() {
-    wbdf = WikibaseDataFetcher.getWikidataDataFetcher();
+    this.fetcher = WikibaseDataFetcher.getWikidataDataFetcher();
   }
 
-  public EntityDocument getOriginEntByQuery(String query) {
-    EntityDocument originDoc = null;
+  /**
+   * Retrieves the origin entity by first searching by title, then widening to search all Entities.
+   *
+   * @param query the query to search for the origin entity
+   * @return an Either object containing either the retrieved EntityDocument or a WikiverseError
+   */
+  public Either<WikiverseError, EntityDocument> getOriginEntityByAny(String query) {
+    Either<WikiverseError, EntityDocument> entityByTitle = fetchEntityByTitle(query);
 
-    try {
-      if (queryIsEntID(query)) {
-        originDoc = getEntById(query);
-      } else {
-        originDoc = getEntByTitle(query);
-      }
-
-      if (originDoc == null) {
-        List<WbSearchEntitiesResult> searchResults = getEntsByAnyMatch(query);
-        if (!searchResults.isEmpty()) {
-          originDoc = getEntById(searchResults.get(0).getEntityId());
-        } else {
-          originDoc = getEntById("Q3454165"); // Default to Kevin Bacon if no results
-        }
-      }
-    } catch (Exception e) {
-      //TODO handle exception
-      System.err.println("Error @ getOriginEntByQuery()" + e.getMessage());
+    if (entityByTitle.isRight() || isFailedApiRequest(entityByTitle.getLeft())) {
+      return entityByTitle;
     }
-    return originDoc;
-  }
 
-  private EntityDocument getEntById(String query) {
-    try {
-      return wbdf.getEntityDocument(query);
-    } catch (Exception e) {
-      //TODO handle exception
-      System.err.println("Error @ getEntById()" + e.getMessage());
-      return null;
+    Either<WikiverseError, WbSearchEntitiesResult> entityByAny = fetchEntityByAny(query);
+
+    if (isNoSuchEntityError(entityByAny.getLeft())) {
+      return fetchEntityById(DEFAULT_FALLBACK_ID);
     }
+
+    return entityByAny.map(this::convertAnyResultToEntityDoc);
   }
 
-  private EntityDocument getEntByTitle(String query) {
-    try {
-      return wbdf.getEntityDocumentByTitle("enwiki", query);
-    } catch (Exception e) {
-      //TODO handle exception
-      System.err.println("Error @ getEntByTitle()" + e.getMessage());
-      return null;
+  //* PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE *//
+  //* PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE *//
+  //* PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE || PRIVATE *//
+
+  private Either<WikiverseError, EntityDocument> fetchEntityByTitle(String query) {
+    Either<WikiverseError, EntityDocument> entityByTitle = fetchEntityWithErrorHandler(
+        () -> fetcher.getEntityDocumentByTitle(WIKI_SITE_KEY, query));
+
+    if (entityByTitle.isLeft()) {
+      return Either.left(entityByTitle.getLeft());
     }
+
+    return entityByTitle.get() == null
+        ? Either.left(
+            new WikiverseError.WikidataServiceError.NoSuchEntityFound("Unable to find entity by title: " + query))
+        : entityByTitle;
   }
 
-  private List<WbSearchEntitiesResult> getEntsByAnyMatch(String query) {
-    List<WbSearchEntitiesResult> searchResults = new ArrayList<>();
-    try {
-      searchResults = wbdf.searchEntities(query, "en");
-    } catch (Exception e) {
-      // TODO: handle exception
-      System.err.println("Error @ getEntsByAnyMatch()" + e.getMessage());
+  private Either<WikiverseError, EntityDocument> fetchEntityById(String query) {
+    return fetchEntityWithErrorHandler(() -> fetcher.getEntityDocument(query));
+  }
+
+  private Either<WikiverseError, List<WbSearchEntitiesResult>> fetchEntitiesBySearch(String query) {
+    return fetchEntityWithErrorHandler(() -> fetcher.searchEntities(query));
+  }
+
+  private Either<WikiverseError, WbSearchEntitiesResult> fetchEntityByAny(String query) {
+    return fetchEntitiesBySearch(query).flatMap(this::handleSearchedEntitiesResults);
+  }
+
+  private Either<WikiverseError, WbSearchEntitiesResult> handleSearchedEntitiesResults(
+      List<WbSearchEntitiesResult> results) {
+    return results.isEmpty()
+        ? Either
+            .left(new WikiverseError.WikidataServiceError.NoSuchEntityFound("Unable to find entity search:" + results))
+        : Either.right(results.get(0));
+  }
+
+  private <T> Either<WikiverseError, T> fetchEntityWithErrorHandler(CheckedFunction0<T> supplier) {
+    return Try.of(supplier)
+        .toEither()
+        .mapLeft(e -> new WikiverseError.WikidataServiceError.ApiRequestFailed(e.getMessage(), e));
+  }
+
+  private boolean entIdIsItemDoc(String s) {
+    return s.matches("[Q]\\d*");
+  }
+
+  private boolean entIdIsPropertyDoc(String s) {
+    return s.matches("[P]\\d*");
+  }
+
+  private boolean isNoSuchEntityError(WikiverseError error) {
+    return error instanceof WikiverseError.WikidataServiceError.NoSuchEntityFound;
+  }
+
+  private boolean isFailedApiRequest(WikiverseError error) {
+    return error instanceof WikiverseError.WikidataServiceError.ApiRequestFailed;
+  }
+
+  private EntityDocument convertAnyResultToEntityDoc(WbSearchEntitiesResult searchResult) {
+    String entId = searchResult.getEntityId();
+    String entIri = searchResult.getConceptUri().replace(entId, "");
+    String entDesc = searchResult.getDescription();
+    String entLabel = searchResult.getLabel();
+    String langCode = "en";
+    EntityDocument doc = null;
+
+    if (entIdIsItemDoc(entId)) {
+      doc = createItemDocument(entId, entIri, entDesc, entLabel, langCode);
+    } else if (entIdIsPropertyDoc(entId)) {
+      doc = createPropertyDocument(entId, entIri, entDesc, entLabel, langCode);
+    } else {
+      throw new IllegalArgumentException("Entity ID is not a valid QID or PID.");
     }
-    return searchResults;
+    return doc;
   }
 
-  private boolean queryIsEntID(String query) {
-    return query.matches("[PQ]\\d*");
+  private EntityDocument createItemDocument(String itemId, String itemIri, String description, String label,
+      String SITE_KEYCode) {
+    ItemIdValue idValue = new ItemIdValueImpl(itemId, itemIri);
+    ItemDocumentBuilder builder = ItemDocumentBuilder.forItemId(idValue);
+    builder.withDescription(description, SITE_KEYCode);
+    builder.withLabel(label, SITE_KEYCode);
+    return builder.build();
+  }
+
+  @SuppressWarnings("deprecation") //DT_PROPERTY is deprecated, required lib is not included in pom
+  private EntityDocument createPropertyDocument(String propertyId, String propertyIri, String description, String label,
+      String SITE_KEYCode) {
+    PropertyIdValue idValue = new PropertyIdValueImpl(propertyId, propertyIri);
+    PropertyDocumentBuilder builder = PropertyDocumentBuilder.forPropertyIdAndDatatype(idValue,
+        DatatypeIdValue.DT_PROPERTY);
+    builder.withDescription(description, SITE_KEYCode);
+    builder.withLabel(label, SITE_KEYCode);
+    return builder.build();
   }
 }
