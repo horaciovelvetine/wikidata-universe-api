389c9566c676a634acd2183af9ebe35e55c8e734
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-26 23:31:13 -0600

    init for debugging

---
+++ src/main/java/edu/velv/wikidata_universe_api/models/utils/Loggable.java
@@ -0,0 +1,29 @@
+package edu.velv.wikidata_universe_api.models.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public interface Loggable extends Timestampable {
+  public static final String LOGFILE_DIR = "src/main/resources/logs";
+
+  public default void print(String msg) {
+    System.out.println(msg);
+  }
+
+  public default void print(String head, String msg) {
+    System.out.println(head + ": " + msg);
+  }
+
+  public default void log(String msg) {
+    try (FileWriter fileWriter = new FileWriter(LOGFILE_DIR + now() + ".log", true);
+        PrintWriter printWriter = new PrintWriter(fileWriter)) {
+      printWriter.println(now() + "::" + msg);
+    } catch (IOException e) {
+      throw new RuntimeException("Error writing to log file: " + e.getMessage());
+    } finally {
+      print("Logged@:: " + LOGFILE_DIR + now() + ".log");
+    }
+  }
+
+}
---
+++ src/main/java/edu/velv/wikidata_universe_api/models/utils/Timestampable.java
@@ -0,0 +1,11 @@
+package edu.velv.wikidata_universe_api.models.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public interface Timestampable {
+  public default String now() {
+    LocalDateTime nDt = LocalDateTime.now();
+    return nDt.format(DateTimeFormatter.ofPattern("HH:mm:ss.SSS"));
+  }
+}
