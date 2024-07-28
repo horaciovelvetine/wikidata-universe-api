3ad30ebdb813f9ab51aad5caee882308c1607c02
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-27 19:09:09 -0600

    Changes some details and responses to help clarify responses

--- src/main/java/edu/velv/wikidata_universe_api/models/err/SessionControllerErrorResponse.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/err/SessionControllerErrorResponse.java
@@ -1,5 +1,7 @@
 package edu.velv.wikidata_universe_api.models.err;
 
+import edu.velv.wikidata_universe_api.models.err.WikiverseError.*;
+
 public record SessionControllerErrorResponse(int status, String message) {
 
   private static final int SERVICE_UNAVAILABLE = 503;
@@ -8,18 +10,18 @@ public record SessionControllerErrorResponse(int status, String message) {
   private static final String NF_MSG = "No entity found with the given query. Please try a different search.";
   private static final int INTERNAL_SERVER_ERR = 500;
   private static final String IS_MSG = "An unexpected error occurred with the MediaWiki API. Please try again later.";
-  private static final int BAD_REQUEST_ERR = 444;
-  private static final String BR_MSG = "An unexpected error occurred.";
+  private static final int DEFAULT_ERR = 404;
+  private static final String DEF_MSG = "Encountered an Unexpected Error, unable to continue. Restart the application.";
 
   public static SessionControllerErrorResponse mapWikiverseErrorToResponse(WikiverseError err) {
     return switch (err) {
-      case WikiverseError.WikidataServiceError.ApiRequestFailed e ->
+      case WikidataServiceError.ApiRequestFailed e ->
         new SessionControllerErrorResponse(SERVICE_UNAVAILABLE, SU_MSG);
-      case WikiverseError.WikidataServiceError.NoSuchEntityFound e ->
+      case WikidataServiceError.NoSuchEntityFound e ->
         new SessionControllerErrorResponse(NOT_FOUND_ERR, NF_MSG);
-      case WikiverseError.WikidataServiceError.UnexpectedError e ->
+      case WikidataServiceError.UnexpectedError e ->
         new SessionControllerErrorResponse(INTERNAL_SERVER_ERR, IS_MSG);
-      default -> new SessionControllerErrorResponse(BAD_REQUEST_ERR, BR_MSG);
+      default -> new SessionControllerErrorResponse(DEFAULT_ERR, DEF_MSG);
     };
   }
 }
