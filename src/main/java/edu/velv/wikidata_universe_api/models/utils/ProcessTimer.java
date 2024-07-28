3ca95e7c9f931272adca0dc14b30916ec9fb794e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-27 19:09:45 -0600

    timing checking util

---
+++ src/main/java/edu/velv/wikidata_universe_api/models/utils/ProcessTimer.java
@@ -0,0 +1,27 @@
+package edu.velv.wikidata_universe_api.models.utils;
+
+public class ProcessTimer {
+  private long startTime;
+  private long endTime;
+
+  public ProcessTimer() {
+    this.startTime = System.currentTimeMillis();
+  }
+
+  public void stop() {
+    this.endTime = System.currentTimeMillis();
+  }
+
+  private long getElapsedTime() {
+    return this.endTime - this.startTime;
+  }
+
+  public String getElapsedTimeFormatted() {
+    long elapsedTime = getElapsedTime();
+    long seconds = elapsedTime / 1000;
+    long minutes = seconds / 60;
+    long hours = minutes / 60;
+    String format = String.format("%02d:%02d:%02d", hours, minutes % 60, seconds % 60);
+    return format + " (" + elapsedTime + "ms).";
+  }
+}
