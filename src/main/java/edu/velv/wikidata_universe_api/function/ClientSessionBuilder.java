ea9766dec4a3a1410ff55bc782e88ead6f74020e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-07-29 17:08:14 -0600

    changes Err importing reflected, and working on underlying tasks

--- src/main/java/edu/velv/wikidata_universe_api/function/ClientSessionBuilder.java
+++ src/main/java/edu/velv/wikidata_universe_api/function/ClientSessionBuilder.java
@@ -2,24 +2,24 @@ package edu.velv.wikidata_universe_api.function;
 
 import java.util.Optional;
 
-import edu.velv.wikidata_universe_api.err.WikiverseError;
+import edu.velv.wikidata_universe_api.err.Err;
 import edu.velv.wikidata_universe_api.models.ClientSession;
 import io.vavr.control.Either;
 
 public class ClientSessionBuilder {
-  public static Either<WikiverseError, ClientSession> initialize(String query, String dimensions) {
+  public static Either<Err, ClientSession> initialize(String query, String dimensions) {
     ClientSession sesh = new ClientSession(query, dimensions);
-    // Fetch Initial Query Data
-    Optional<WikiverseError> fetchInitQueryTask = sesh.wikidataManager().fetchInitQueryData();
+    Optional<Err> fetchInitQueryTask = sesh.wikidataManager().fetchInitQueryData();
     if (fetchInitQueryTask.isPresent()) {
       return Either.left(fetchInitQueryTask.get());
     }
-    //? optionally... => earlier response return, and fetchRelatedDataTask is run next || optomistically 
-    // Fetch Related Data
-    Optional<WikiverseError> fetchRelatedDataTask = sesh.wikidataManager().fetchRelatedDataWithTimeout();
+
+    Optional<Err> fetchRelatedDataTask = sesh.wikidataManager().fetchWithTimeoutTest();
+    
     if (fetchRelatedDataTask.isPresent()) {
       return Either.left(fetchRelatedDataTask.get());
     }
+
     //TODO: below...
     // * verify that graphset has been populated
     // * initialize layout coords for set
