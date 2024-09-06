9280f5b7d56ec7123888baf134dbc91eebd428e0
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-05 20:39:30 -0600

    init refactor of single fetch task completed, untested

--- src/main/java/edu/velv/wikidata_universe_api/models/wikidata/WikidataManager.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/wikidata/WikidataManager.java
@@ -6,7 +6,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -18,6 +17,7 @@ import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
 import edu.velv.wikidata_universe_api.errors.Err;
 import edu.velv.wikidata_universe_api.errors.WikidataServiceError.FetchRelatedWithTimeoutError;
 import edu.velv.wikidata_universe_api.models.ClientRequest;
+import edu.velv.wikidata_universe_api.models.Constables;
 import edu.velv.wikidata_universe_api.models.RequestPayload;
 import edu.velv.wikidata_universe_api.models.jung_ish.Edge;
 import edu.velv.wikidata_universe_api.models.jung_ish.Vertex;
@@ -26,9 +26,6 @@ import edu.velv.wikidata_universe_api.utils.ProcessTimer;
 import io.vavr.control.Either;
 
 public class WikidataManager implements Loggable {
-  protected static final Integer FETCH_TIMEOUT_MAX = 1;
-  protected static final TimeUnit FETCH_TIMEOUT_UNIT = TimeUnit.MINUTES;
-  protected static final Integer N_DEPTH_MAX = 1;
   protected Integer n;
   protected Set<Property> properties;
   protected FetchQueue queue;
@@ -67,9 +64,9 @@ public class WikidataManager implements Loggable {
    * @return An Optional<Err> if one is encountered
    */
   public Optional<Err> fetchRelatedWithTimeout() {
-    Future<Optional<Err>> fetchTaskFuture = timeoutExecutor.submit(this::fetchRelatedTask);
+    Future<Optional<Err>> fetchTaskFuture = timeoutExecutor.submit(this::fetchRelatedTaskSingleRunner);
     try {
-      return fetchTaskFuture.get(FETCH_TIMEOUT_MAX, FETCH_TIMEOUT_UNIT);
+      return fetchTaskFuture.get(Constables.WD_TIMEOUT_LIMIT, Constables.WD_TIMEOUT_UNIT);
     } catch (Exception e) {
       return Optional.of(new FetchRelatedWithTimeoutError("fetchRelatedData timed out", e));
     } finally {
@@ -121,74 +118,122 @@ public class WikidataManager implements Loggable {
   //=====================================================================================================================>
   //=====================================================================================================================>
 
-  private Optional<Err> fetchRelatedTask() {
-    ProcessTimer taskTimer = new ProcessTimer();
-    while (n <= N_DEPTH_MAX) {
-      try {
-        ProcessTimer fetchTimer = new ProcessTimer();
+  // should remove the duality of fetching dates using another mechanism
+  private Optional<Err> fetchRelatedTaskSingleRunner() {
+    while (n <= Constables.WD_MAX_N_FETCH_DEPTH) {
+      List<String> tgtBatch = queue.getTargetBatchByPriority(n);
+      boolean isEntBatch = tgtBatch.getFirst().matches(Regex.ENT_ID);
+      Optional<Err> batchProcTask;
 
-        List<String> dateBatch = queue.getDateTargetBatch(n);
-        List<String> qBatch = queue.getEntTargetBatch(n);
-
-        Either<Err, Map<String, Either<Err, EntityDocument>>> qResults = wikidataApi.fetchEntitiesByIdList(qBatch);
-        Either<Err, Map<String, Either<Err, WbSearchEntitiesResult>>> dateResults = wikidataApi
-            .fetchEntitiesByDateList(dateBatch);
-
-        if (qResults.isLeft()) {
-          return Optional.of(qResults.getLeft());
-        }
-        // Collect fetch details for eval
-        // === === === === === === === === === === === ===
-        fetchTimer.stop(); // stop time for processing 
-        int totalFetched = 0;
-        totalFetched += qResults.get().entrySet().size();
-        totalFetched += dateResults.get().entrySet().size();
-        logFetch(fetchTimer.getElapsedTimeFormatted() + " :: " + totalFetched);
-        // === === === === === === === === === === === ===
-
-        handleDateResults(dateResults.get());
-        handleFetchResults(qResults.get());
-
-        if (queue.isEmptyAtNDepth(n)) {
-          n += 1;
-        }
-      } catch (Exception e) {
-        return Optional.of(new FetchRelatedWithTimeoutError("@fetchRelatedTask()", e));
+      if (isEntBatch) {
+        batchProcTask = fetchAndHandleEntities(tgtBatch);
+      } else {
+        batchProcTask = fetchAndHandleDates(tgtBatch);
       }
-      print(session.details());
-    }
-    // === === === === === === === === === === === ===
-    taskTimer.stop();
-    logFetch(session.details());
-    logFetch(taskTimer.getElapsedTimeFormatted() + "\n");
-    // === === === === === === === === === === === ===
 
+      if (batchProcTask.isPresent()) {
+        // exits with only API error, NoResultsErrs are relegated to invalid
+        return batchProcTask;
+      }
+
+      if (queue.isEmptyAtNDepth(n)) {
+        n += 1;
+      }
+      // ends loop...
+    }
+    return Optional.empty();
+  }
+
+  private Optional<Err> fetchAndHandleEntities(List<String> batch) {
+    Either<Err, Map<String, Either<Err, EntityDocument>>> results = wikidataApi.fetchEntitiesByIdList(batch);
+    if (results.isLeft()) {
+      return Optional.of(results.getLeft());
+    }
+    handleFetchResults(results.get());
+    return Optional.empty();
+  }
+
+  private Optional<Err> fetchAndHandleDates(List<String> batch) {
+    Either<Err, Map<String, Either<Err, WbSearchEntitiesResult>>> results = wikidataApi.fetchEntitiesByDateList(batch);
+    if (results.isLeft()) {
+      return Optional.of(results.getLeft());
+    }
+    handleDateResults(results.get());
     return Optional.empty();
   }
 
   private void handleFetchResults(Map<String, Either<Err, EntityDocument>> results) {
-    for (Entry<String, Either<Err, EntityDocument>> result : results.entrySet()) {
-      if (result.getValue().isLeft()) {
-        log(buildInvalidLogString(result.getKey()));
-        queue.fetchInvalidCleanup(result.getKey());
+    if (results != null) {
+      for (Entry<String, Either<Err, EntityDocument>> result : results.entrySet()) {
+        if (result.getValue().isLeft()) {
+          log(buildInvalidLogString(result.getKey()));
+          queue.fetchInvalidCleanup(result.getKey());
+        }
+        entProc.processWikiEntDocument(result.getValue().get());
+        queue.fetchSuccessCleanup(result.getKey());
       }
-      entProc.processWikiEntDocument(result.getValue().get());
-      queue.fetchSuccessCleanup(result.getKey());
     }
   }
 
   private void handleDateResults(Map<String, Either<Err, WbSearchEntitiesResult>> results) {
-    for (Entry<String, Either<Err, WbSearchEntitiesResult>> result : results.entrySet()) {
-      if (result.getValue().isLeft()) {
-        log(buildInvalidLogString(result.getKey()));
-        queue.fetchInvalidCleanup(result.getKey());
+    if (results != null) {
+      for (Entry<String, Either<Err, WbSearchEntitiesResult>> result : results.entrySet()) {
+        if (result.getValue().isLeft()) {
+          log(buildInvalidLogString(result.getKey()));
+          queue.fetchInvalidCleanup(result.getKey());
+        }
+        entProc.processSearchEntResult(result.getValue().get());
+        queue.fetchSuccessCleanup(result.getKey());
       }
-      entProc.processSearchEntResult(result.getValue().get());
-      queue.fetchSuccessCleanup(result.getKey());
     }
   }
 
   protected String buildInvalidLogString(String tgtQuery) {
     return "Invalid/Error: (" + tgtQuery + ") query.";
   }
+
+  // private Optional<Err> fetchRelatedTask() {
+  //   ProcessTimer taskTimer = new ProcessTimer();
+  //   while (n <= Constables.WD_MAX_N_FETCH_DEPTH) {
+  //     try {
+  //       ProcessTimer fetchTimer = new ProcessTimer();
+
+  //       List<String> dateBatch = queue.getDateTargetBatch(n);
+  //       List<String> qBatch = queue.getEntTargetBatch(n);
+
+  //       Either<Err, Map<String, Either<Err, EntityDocument>>> qResults = wikidataApi.fetchEntitiesByIdList(qBatch);
+  //       Either<Err, Map<String, Either<Err, WbSearchEntitiesResult>>> dateResults = wikidataApi
+  //           .fetchEntitiesByDateList(dateBatch);
+
+  //       if (qResults.isLeft()) {
+  //         return Optional.of(qResults.getLeft());
+  //       }
+  //       // Collect fetch details for eval
+  //       // === === === === === === === === === === === ===
+  //       fetchTimer.stop(); // stop time for processing 
+  //       int totalFetched = 0;
+  //       totalFetched += qResults.get().entrySet().size();
+  //       totalFetched += dateResults.get().entrySet().size();
+  //       logFetch(fetchTimer.getElapsedTimeFormatted() + " :: " + totalFetched);
+  //       // === === === === === === === === === === === ===
+
+  //       handleDateResults(dateResults.get());
+  //       handleFetchResults(qResults.get());
+
+  //       if (queue.isEmptyAtNDepth(n)) {
+  //         n += 1;
+  //       }
+  //     } catch (Exception e) {
+  //       return Optional.of(new FetchRelatedWithTimeoutError("@fetchRelatedTask()", e));
+  //     }
+  //     print(session.details());
+  //   }
+  //   // === === === === === === === === === === === ===
+  //   taskTimer.stop();
+  //   logFetch(session.details());
+  //   logFetch(taskTimer.getElapsedTimeFormatted() + "\n");
+  //   // === === === === === === === === === === === ===
+
+  //   return Optional.empty();
+  // }
 }
