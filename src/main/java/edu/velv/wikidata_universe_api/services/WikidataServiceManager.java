3c9e2d17ed3169daad8173f5a93addbb06dbf88e
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-15 15:42:16 -0600

    init new service model with primary tasks

---
+++ src/main/java/edu/velv/wikidata_universe_api/services/WikidataServiceManager.java
@@ -0,0 +1,105 @@
+package edu.velv.wikidata_universe_api.services;
+
+import java.util.Optional;
+
+import io.vavr.control.Either;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
+
+import edu.velv.wikidata_universe_api.ClientRequest;
+import edu.velv.wikidata_universe_api.models.Property;
+import edu.velv.wikidata_universe_api.models.Vertex;
+import edu.velv.wikidata_universe_api.errors.Err;
+
+@Service
+public class WikidataServiceManager {
+  @Autowired
+  private FetchBroker api;
+
+  @Autowired
+  private EntDocProc docProc;
+
+  /**
+   * Uses the initial query value to find the target of the request and then gather the initial data
+   * used to construct a Graph (layout), then return the initial results for the client. Results in a
+   * single vertex on screen, with further fetch details stored for a subsequent request task. 
+   * 
+   * @param ClientRequest
+   * @return an Err(or) if one was encountered during execution
+   */
+  public Optional<Err> fetchInitQueryDataTask(ClientRequest req) {
+    Either<Err, EntityDocument> initQueryDataTask = api.fetchEntityByAnyQueryMatch(req.query());
+
+    if (initQueryDataTask.isLeft()) {
+      return Optional.of(initQueryDataTask.getLeft());
+    }
+
+    EntityDocument entDoc = initQueryDataTask.get();
+    return ingestInitQueryDataDoc(entDoc, req);
+  }
+
+  /**
+   * Fetch currently unknown values from the Wikidata API to complete the details of the Graphset, called
+   * as a subsequent request from the client to ensure valid connection with Client (application) before initiating 
+   * larger request load on Wikidata API.
+   * 
+   * @param ClientRequest
+   * @return an Err<er> if one was encountered during execution
+   */
+  public Optional<Err> fetchIncompleteDataTask(ClientRequest clientRequest) {
+    //todo
+    while (!clientRequest.graph().allDataFetched()) {
+      
+    }
+
+    return Optional.empty();
+  }
+
+  //=====================================================================================================================>
+  //=====================================================================================================================>
+  //=====================================================================================================================>
+
+  /**
+   * Uses successfully fetched data from Wikidata to compose data storage objects and add
+   * them back to the original ClientRequest.
+   * 
+   * @param EntityDocument
+   * @param ClientRequest
+   * @return an Err(or) if one was encountered carrying out task
+   */
+  private Optional<Err> ingestInitQueryDataDoc(EntityDocument entDoc, ClientRequest req) {
+    Optional<Vertex> isVert = docProc.attemptVertexCreateUnkownTypeEntDoc(entDoc);
+    if (isVert.isPresent()) {
+      Vertex itemDoc = isVert.get();
+      req.graph().addVertex(itemDoc);
+      ingestEdgesFromNewVertex(entDoc, req);
+    }
+    return Optional.empty();
+  }
+
+  /**
+   * Called after successful creation of a new Vertex to create edges using the Statements
+   * from the originating EntityDocument (ItemDocument). Each Edge is used to create an incomplete
+   * Vertex & Property which are *added* along with the new Edge.
+   *
+   * @param EntityDocument
+   * @param ClientRequest 
+   */
+  private void ingestEdgesFromNewVertex(EntityDocument entDoc, ClientRequest req) {
+    docProc.createRelatedEdgesFromStatements(entDoc).forEach(newEdge -> {
+      Property newProp = new Property();
+      Vertex tgtVert = new Vertex();
+
+      newProp.id(newEdge.propertyId());
+      tgtVert.label(newEdge.label()); //? one of these is null depending on 
+      tgtVert.id(newEdge.tgtId()); //? if this edge defines a date target
+
+      req.graph().addEdge(newEdge);
+      req.graph().addProperty(newProp);
+      req.graph().addVertex(tgtVert);
+    });
+
+  }
+}
