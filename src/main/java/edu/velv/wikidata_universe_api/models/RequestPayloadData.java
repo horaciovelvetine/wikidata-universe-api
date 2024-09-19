9b2e1dc069de99d603f5c0544225006fb27f4af2
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-09-18 22:48:12 -0600

    fixes list, unsure why I was still on collection

--- src/main/java/edu/velv/wikidata_universe_api/models/RequestPayloadData.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/RequestPayloadData.java
@@ -2,11 +2,11 @@ package edu.velv.wikidata_universe_api.models;
 
 import java.awt.Dimension;
 
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Record stores the details of an incoming Client Request as they are originally recieved 
  */
-public record RequestPayloadData(String query, Dimension dimensions, Collection<Vertex> vertices,
-    Collection<Edge> edges, Collection<Property> properties) {
+public record RequestPayloadData(String query, Dimension dimensions, List<Vertex> vertices,
+        List<Edge> edges, List<Property> properties) {
 }
