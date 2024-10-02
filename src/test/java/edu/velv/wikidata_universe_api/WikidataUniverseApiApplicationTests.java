2ef1987a4776d219dc4d19e0db35ba2dd5d169ab
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-01 23:46:18 -0600

    adds init tests for config values working

--- src/test/java/edu/velv/wikidata_universe_api/WikidataUniverseApiApplicationTests.java
+++ src/test/java/edu/velv/wikidata_universe_api/WikidataUniverseApiApplicationTests.java
@@ -4,28 +4,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import edu.velv.wikidata_universe_api._TestUtils.TestDataBuilders;
-import edu.velv.wikidata_universe_api._TestUtils.FailedTestMessageTemplates;
-import edu.velv.wikidata_universe_api.services.EntDocProc;
+import edu.velv.wikidata_universe_api._Utils.FailedTestMessageTemplates;
 import edu.velv.wikidata_universe_api.services.FR3DConfig;
-import edu.velv.wikidata_universe_api.services.FetchBroker;
 import edu.velv.wikidata_universe_api.services.WikidataServiceManager;
 
 @SpringBootTest
-@AutoConfigureMockMvc
-public class WikidataUniverseApiApplicationTests implements FailedTestMessageTemplates, TestDataBuilders {
+public class WikidataUniverseApiApplicationTests implements FailedTestMessageTemplates {
   private final String local = "@WikidataUniverseApiApplicationTests ";
   @Autowired
   private WikidataServiceManager wikidataSrvcMngr;
-
-  @Autowired
-  private EntDocProc entDocProc;
-
-  @Autowired
-  private FetchBroker fetchBroker;
 
   @Autowired
   private FR3DConfig fr3dConfig;
@@ -33,18 +22,20 @@ public class WikidataUniverseApiApplicationTests implements FailedTestMessageTem
   @Test
   public void spring_boot_context_loads_correctly() {
     checkFR3DConfigValuesArePresent();
-    assertNotNull(wikidataSrvcMngr, local + unableToFind + "service/WikidataServiceManager @Bean");
-    assertNotNull(wikidataSrvcMngr.docProc, local + unableToFind + "service/EntDocProc @Bean");
-    assertNotNull(wikidataSrvcMngr.api, local + unableToFind + "service/FetchBroker @Bean");
-
+    assertNotNull(wikidataSrvcMngr, local + unableToFind + "WikidataServiceManager @Bean");
+    assertNotNull(wikidataSrvcMngr.docProc, local + unableToFind + "EntDocProc @Bean");
+    assertNotNull(wikidataSrvcMngr.api, local + unableToFind + "FetchBroker @Bean");
   }
 
+  /**
+   * Individually test for each of the values pulled from application.properties on startup from the FR3DConfig
+   */
   private void checkFR3DConfigValuesArePresent() {
-    assertNotNull(fr3dConfig.targetDensity(), local + unableToFind + "Target Density value");
-    assertNotNull(fr3dConfig.repMult(), local + unableToFind + "Repulsion Multiplier value");
-    assertNotNull(fr3dConfig.attrMult(), local + unableToFind + "Attraction Multiplier value");
-    assertNotNull(fr3dConfig.tempMult(), local + unableToFind + "Temperature Multiplier value");
-    assertNotNull(fr3dConfig.maxIters(), local + unableToFind + "Max Layout Iterations limit value");
-    assertNotNull(fr3dConfig.maxIterMvmnt(), local + unableToFind + "Max per-Iteration movement value");
+    assertNotNull(fr3dConfig.targetDensity(), local + unableToFind + "Target Density" + val);
+    assertNotNull(fr3dConfig.repMult(), local + unableToFind + "Repulsion Multiplier" + val);
+    assertNotNull(fr3dConfig.attrMult(), local + unableToFind + "Attraction Multiplier" + val);
+    assertNotNull(fr3dConfig.tempMult(), local + unableToFind + "Temperature Multiplier" + val);
+    assertNotNull(fr3dConfig.maxIters(), local + unableToFind + "Max Layout Iterations limit" + val);
+    assertNotNull(fr3dConfig.maxIterMvmnt(), local + unableToFind + "Max per-Iteration movement" + val);
   }
 }
