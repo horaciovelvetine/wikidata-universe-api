0eff9c951e0dbc65757588a94814a63ca81ab1fa
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-10-06 19:40:03 -0600

    refactor to split more clearly along ingest monster methods and be more readable
    adds injectable dependency constructor for testing

--- src/main/java/edu/velv/wikidata_universe_api/services/WikidataServiceManager.java
+++ src/main/java/edu/velv/wikidata_universe_api/services/WikidataServiceManager.java
@@ -33,6 +33,14 @@ public class WikidataServiceManager implements Printable {
   }
 
   /**
+   * An injectable version for testing services
+   */
+  public WikidataServiceManager(FetchBroker api, EntDocProc docProc) {
+    this.api = api;
+    this.docProc = docProc;
+  }
+
+  /**
    * Uses the initial query value to find the target of the request and then gather the initial data
    * used to construct a Graph and return the initial results for the client. This doesn't include any
    * layout data placing a single vertex (the origin) at a new Point3D (0,0,0)
@@ -92,7 +100,6 @@ public class WikidataServiceManager implements Printable {
    */
   private void ingestEntityDocAsVertexWithEdges(EntityDocument entDoc, ClientRequest req) {
     Optional<Vertex> isVert = docProc.createVertexFromUnknownEntDoc(entDoc, api.enLangKey());
-
     if (isVert.isPresent()) {
       Vertex vert = isVert.get();
       if (req.graph().hasNoExistingVertices())
@@ -100,9 +107,6 @@ public class WikidataServiceManager implements Printable {
 
       req.graph().addVertex(vert);
       ingestEdgesFromNewVertex(entDoc, req);
-
-    } else {
-      print("An unknown Entity Document has been encountered");
     }
   }
 
@@ -132,38 +136,45 @@ public class WikidataServiceManager implements Printable {
    * Fetches details for a batch of Wikidata Entities where only the ID value is currently known, then updates the existing 
    * Vertex or Property with the details of a successful fetch, or if there was no matching result removes mention of this value
    * from the Graphset to prevent bad data.
+   * 
+   * @apiNote - empty batches are only checked on dates because empty entity batches triggers the beginning of fetching dates
    */
 
   private Optional<Err> ingestUnfetchedTargetIdBatchData(List<String> idBatch, ClientRequest req,
       IncompleteDataQueue queue) {
-    Either<Err, Map<String, Either<Err, EntityDocument>>> idResults = api.fetchEntitiesByIdList(idBatch);
+    return api.fetchEntitiesByIdList(idBatch).fold(
+        err -> {
+          return Optional.of(err);
+        },
+        entResults -> {
+          for (Entry<String, Either<Err, EntityDocument>> result : entResults.entrySet()) {
+            removeEntFetchedTarget(queue, result, req);
+            EntityDocument resDoc = result.getValue().get();
 
-    if (idResults.isLeft()) {
-      return Optional.of(idResults.getLeft());
+            if (resDoc instanceof ItemDocumentImpl) {
+              Vertex existingVert = req.graph().getVertexById(result.getKey()).get();
+              existingVert.updateUnfetchedValues((ItemDocumentImpl) resDoc, api.enLangKey());
+
+            } else if (resDoc instanceof PropertyDocument) {
+              Property existingProp = req.graph().getPropertyById(result.getKey()).get();
+              existingProp.updateUnfetchedValues((PropertyDocument) resDoc, api.enLangKey());
+
+            }
+          }
+          return Optional.empty();
+        });
+  }
+
+  /**
+  * Uses the provided fetch result to remove it first from the incomplete queue, then checking for an NoSuchEntitiesErr removes any data which mentions
+  * the non-existent vertex.
+  */
+  private void removeEntFetchedTarget(IncompleteDataQueue queue, Entry<String, Either<Err, EntityDocument>> entry,
+      ClientRequest request) {
+    queue.removeFromQueue(entry.getKey());
+    if (entry.getValue().isLeft()) {
+      request.graph().removeInvalidSearchResultFromData(entry.getKey());
     }
-
-    for (Entry<String, Either<Err, EntityDocument>> result : idResults.get().entrySet()) {
-      queue.removeFromQueue(result.getKey());
-
-      if (result.getValue().isLeft() || result.getValue().get() == null) { // NoSuchResultsErr removes tgt mention from graphset
-        req.graph().removeInvalidSearchResultFromData(result.getKey());
-      }
-
-      EntityDocument resDoc = result.getValue().get();
-      Optional<Vertex> isVert = docProc.createVertexFromUnknownEntDoc(resDoc, api.enLangKey());
-
-      if (isVert.isPresent()) {
-        req.graph().getVertexById(result.getKey()).get()
-            .updateUnfetchedValues((ItemDocumentImpl) resDoc, this.api.enLangKey());
-      } else if (resDoc instanceof PropertyDocument) { // is a Property is only other possibility
-        req.graph().getPropertyById(result.getKey()).get()
-            .updateUnfetchedValues((PropertyDocument) resDoc, this.api.enLangKey());
-      } else {
-        print("Encountered an unknown doc type while ingesting related entIDBatch");
-      }
-      ingestEdgesFromNewVertex(resDoc, req);
-    }
-    return Optional.empty();
   }
 
   /**
@@ -173,28 +184,36 @@ public class WikidataServiceManager implements Printable {
    */
   private Optional<Err> ingestUnfetchedTargetDateBatchData(List<String> dateBatch, ClientRequest req,
       IncompleteDataQueue queue) {
-    if (dateBatch.isEmpty())
+    if (dateBatch.isEmpty()) {// only fetch details when the batch isn't empty
       return Optional.empty();
-
-    Either<Err, Map<String, Either<Err, WbSearchEntitiesResult>>> dateResults = api.fetchEntitiesByDateList(dateBatch);
-    if (dateResults.isLeft()) {
-      return Optional.of(dateResults.getLeft());
     }
 
-    for (Entry<String, Either<Err, WbSearchEntitiesResult>> result : dateResults.get().entrySet()) {
-      queue.removeFromQueue(result.getKey());
+    return api.fetchEntitiesByDateList(dateBatch).fold(errResults -> {
+      return Optional.of(errResults);
+    }, dateResults -> {
+      for (Entry<String, Either<Err, WbSearchEntitiesResult>> result : dateResults.entrySet()) {
+        removeDateFetchedTarget(queue, result, req);
 
-      if (result.getValue().isLeft()) {
-        req.graph().removeInvalidSearchResultFromData(result.getKey());
+        Optional<Vertex> srcVertex = req.graph().getVertexById(result.getKey());
+        if (srcVertex.isPresent()) {
+          srcVertex.get().updateUnfetchedValues(result.getValue().get());
+        }
       }
+      return Optional.empty();
+    });
+  }
 
-      Optional<Vertex> srcVertex = req.graph().getVertexById(result.getKey());
-
-      if (srcVertex.isPresent()) {
-        srcVertex.get().updateUnfetchedValues(result.getValue().get());
-      }
+  /**
+   * Uses the provided fetch result to remove it first from the incomplete queue, then checking for an NoSuchEntitiesErr removes any data which mentions
+   * the non-existent vertex.
+   */
+  private void removeDateFetchedTarget(IncompleteDataQueue queue,
+      Entry<String, Either<Err, WbSearchEntitiesResult>> entry, ClientRequest request) {
+    queue.removeFromQueue(entry.getKey());
+    if (entry.getValue().isLeft()) {
+      request.graph().removeInvalidSearchResultFromData(entry.getKey());
     }
-    return Optional.empty();
+
   }
 
   /**
