4add22fd0f88fdd21c175c132390e3754f43fe3d
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-11-14 13:36:42 -0700

    places data back in the named dir and points at the target build location the data ends up - working locally

--- src/main/java/edu/velv/wikidata_universe_api/models/TutorialRequest.java
+++ src/main/java/edu/velv/wikidata_universe_api/models/TutorialRequest.java
@@ -27,7 +27,7 @@ public class TutorialRequest implements Loggable {
   private final WikidataServiceManager wikidata;
   private Map<String, AboutSlide> slideData;
 
-  private static final String SLIDE_DATA_PATH = "src/main/java/edu/velv/wikidata_universe_api/models/tutorial_slide_data.json";
+  private static final String SLIDE_DATA_PATH = "target/classes/data/tutorial_slide_data.json";
 
   public TutorialRequest(WikidataServiceManager wd) {
     this.message = null;
